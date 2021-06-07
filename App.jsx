991caa9c0f054446007cf47ba14fa885774c02fe
Vinayak0209 <85481596+Vinayak0209@users.noreply.github.com>
2021-06-07 13:32:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Box from "./Note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="Note">
+      <h1>Javascript & ReactJs</h1>
+      <p>
+        I Joint th 7 days bootcamp and its understand esily thankyou shaurya
+        singha to give us valuable knowleged.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
